f652a4de3e36fc081b9c3eb65c8f9b18369f148c
alencar-felipe <alencar.felipe.paiva@gmail.com>
2022-01-07 15:31:23 -0300

    simple wine image

--- wine/helpers/docker.sh
+++ wine/helpers/docker.sh
@@ -7,5 +7,5 @@ fi
 
 xhost +
 
-alias wine="docker run --rm -it -v \$(pwd):/home/runtime -v /tmp/.X11-unix:/tmp/.X11-unix \
--e DISPLAY=$DISPLAY wine"
+alias wine="docker run --rm -it -v \$(pwd):/workdir -v \
+/tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY wine"
