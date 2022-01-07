016258c7f665d331edb672e62cc16219d450799f
alencar-felipe <alencar.felipe.paiva@gmail.com>
2022-01-07 14:40:06 -0300

    moving ltspice to private repo

--- ltspice/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-groupadd -g $GID runtimegroup
-useradd --create-home runtime -u $UID -g $GID
-
-echo "wheel ALL=(ALL) ALL" >> /etc/sudoers
-echo "runtime ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-su -c 'wine /LTspiceXVII.exe' runtime
