46a43edf4cc401bb06ecda9f17d24c24ec30e335
alencar-felipe <alencar.felipe.paiva@gmail.com>
2024-10-08 00:45:53 +0200

    tag bug fix in build

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # docker-images
 
-![CI](https://github.com/alencar-felipe/docker-images/actions/workflows/ci.yml/badge.svg)
+[![build](https://github.com/alencar-felipe/docker-images/actions/workflows/build.yaml/badge.svg?branch=main)](https://github.com/alencar-felipe/docker-images/actions/workflows/build.yaml)
 
 This repository contains my personal Docker images for various applications
 and tools. Each Docker image is stored in its own directory.
--- generate-workflow.py
+++ generate-workflow.py
@@ -18,27 +18,20 @@ name: build
 """
 
 JOB_TEMPLATE = """
-name: {image_name}
+name: {job_name}
 runs-on: ubuntu-latest
 env:
   DOCKERHUB_USERNAME: ${{{{ vars.DOCKERHUB_USERNAME }}}}
 
 steps:
   - name: Checkout code
-    uses: actions/checkout@v3
+    uses: actions/checkout@v4
 
   - name: Set up Buildx
-    uses: docker/setup-buildx-action@v2
-
-  - name: Cache Docker layers
-    uses: actions/cache@v3
-    with:
-      path: /tmp/.buildx-cache
-      key: ${{{{ runner.os }}}}-buildx-${{{{ github.sha }}}}
-      restore-keys: ${{{{ runner.os }}}}-buildx-
+    uses: docker/setup-buildx-action@v3
 
   - name: Log in to Docker Hub
-    uses: docker/login-action@v2
+    uses: docker/login-action@v3
     with:
       username: ${{{{ vars.DOCKERHUB_USERNAME }}}}
       password: ${{{{ secrets.DOCKERHUB_PASSWORD }}}}
@@ -46,12 +39,10 @@ steps:
   - name: Build and push image
     run: |
         GIT_HASH=$(git rev-parse --short HEAD)
-        docker buildx build \
-          --cache-from type=local,src=/tmp/.buildx-cache \
-          --cache-to type=local,dest=/tmp/.buildx-cache \
-          --push \
+        docker buildx build --push \
+          {args} \
           -t ${{DOCKERHUB_USERNAME}}/{image_name}:{image_tag} \
-          -t ${{DOCKERHUB_USERNAME}}/{image_name}:${{GIT_HASH}}-{image_tag} \
+          -t ${{DOCKERHUB_USERNAME}}/{image_name}:{image_tag}-${{GIT_HASH}} \
           ./{image_name}
 
 """
@@ -87,22 +78,25 @@ def image_jobs(image_name: str) -> dict:
     with open(index_path, "r") as file:
       index = yaml.safe_load(file)
 
-  tags: dict = index.get("tags", {"latest": []})
-  needs: list = index.get("needs", [])
+  tags: dict[str] = index.get("tags", {"latest": []})
+  needs: list[str] = index.get("needs", [])
 
   jobs = {}
   for tag, args in tags.items():
 
+    job_name = get_job_name(image_name, tag)
+
     job = yaml.safe_load(JOB_TEMPLATE.format(
+      job_name=job_name,
       image_name=image_name,
       image_tag=tag,
       args=args_fmt(args)
     ))
 
     if needs:
-      job["needs"] = needs.copy()
+      job["needs"] = [get_job_name(need) for need in needs]
 
-    jobs[image_name] = job
+    jobs[job_name] = job
 
   return jobs
 
@@ -112,10 +106,16 @@ def args_fmt(args: dict) -> str:
     return ""
 
   return "".join(
-      f"--build_arg {key}=\"{value}\" "
+      f"--build-arg {key}=\"{value}\" "
       for key, value in args.items()
   )
 
+def get_job_name(text, tag="latest"):
+  if ":" not in text:
+    text = f"{text}:{tag}"
+  for c in ":.":
+    text = text.replace(c, "-")
+  return text
 
 if __name__ == "__main__":
     main()
